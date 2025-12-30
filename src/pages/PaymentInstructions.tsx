a8ecd3e261d8e16f403a8f2a94a91183e1ae45b6
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-12-30 09:20:45 +0100

    Update PaymentInstructions.tsx

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "8154017339";
-  const bankName = "AKERELE BAMITALE";
-  const accountName = "PALMPAY BANK";
+  const accountNumber = "5070811364";
+  const bankName = "Abraham Olanegan";
+  const accountName = "Moniepoint MFB";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
